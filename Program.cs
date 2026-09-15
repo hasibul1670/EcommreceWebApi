7004d0b2c672b3de9ef1d5bd17eabdc23d8c0ef8
hasibul1670 <hasibulislam1670@gmail.com>
2026-09-15 23:02:32 +0600

    learn MapGet

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
+app.MapGet("/", () => { return "based URL.."; });
+app.MapGet("/hello", () =>
+{
+    return "Hello from get endpoint:(/hello)";
+});
+
+app.MapGet("/p/{id}", (int id) =>
+{
+    return $"hello text p {id}";
+});
 app.Run();
+
+
 
