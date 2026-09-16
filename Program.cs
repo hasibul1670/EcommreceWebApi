887756d9ed6a29e46a4e165fbe0b570f49bef7cf
hasibul1670 <hasibulislam1670@gmail.com>
2026-09-16 21:21:01 +0600

    learn :: Connect swagger UI

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
+using Microsoft.AspNetCore.WebSockets;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment()) // development e check korbe
+{
+    app.UseSwagger();//middleware
+    app.UseSwaggerUI();
+}
+
+
 app.UseHttpsRedirection();
 app.MapGet("/", () => { return "based URL.."; });
 app.MapGet("/hello", () =>
@@ -11,7 +23,7 @@ app.MapGet("/p/{id}", (int id) =>
 {
     return $"hello text p {id}";
 });
+app.MapGet("/health", () => Results.Ok(new { Status = "Healthy", Timestamp = DateTime.UtcNow }));
+app.MapPost("/post", () => { return "Hello POST Method"; });
 app.Run();
-
-
 
